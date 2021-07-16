f6ab52fe35e637070ae49ce8c62721a61b8ec700
Bruno Vercelli <bruno.vercelli@hotmail.com>
2021-07-16 17:20:20 -0300

    Implement redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,20 @@
 import { createStore, combineReducers } from 'redux';
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
+
 import calculatorReducer from '../reducers/Calculator';
 import products from '../reducers/Products';
 
 const rootReducer = combineReducers({
     calculator: calculatorReducer,
     products: products
-})
+});
 
-const store = createStore(rootReducer);
+const persistedReducer = persistReducer({
+    key: 'root',
+    storage //importado
+}, rootReducer);
 
-export default store;
+export const store = createStore(persistedReducer);
+export const persistedStore = persistStore(store);
