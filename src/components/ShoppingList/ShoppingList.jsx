56733a1b24a7f5c525a04d6fb404fa8c7ecf8da6
Bruno Vercelli <bruno.vercelli@hotmail.com>
2021-07-16 17:03:28 -0300

    improvements on shopping list

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Wrapper, Title, Array } from './ShoppingList.styles'
 import Checkbox from '../../shared/Checkbox'
-// import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-function ShoppingList ({ title, products, onToggle }) {
-  // const productsFromRedux = useSelector(state => state.products);
+function ShoppingList ({ title, onToggle, displayOnlySelected }) {
+  const products = useSelector(state => { 
+    return (
+      displayOnlySelected ? state.products.filter(product => product.checked) : state.products //se verdadeira, pega os produtos selecionados, se não pega todos
+    )
+  });
 
   return <Wrapper>
     <Title>
